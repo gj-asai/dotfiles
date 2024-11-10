0bf4690abf95b2d3f96b102dd33f4430e6333927
Gustavo Asai <gustavojun.asai@gmail.com>
2024-11-10 05:39:30 +0100

    replace vim-gitgutter by gitsigns

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -38,8 +38,9 @@ return {
 
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>gp", builtin.git_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
 		end,
 	},
 	{
