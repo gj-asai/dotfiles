62dc22a080030572a70032a97e2a5041814a7547
Gustavo Asai <gustavojun.asai@gmail.com>
2024-11-20 02:54:39 +0100

    fix nvim keymap

--- dot_config/nvim/lua/remaps.lua
+++ dot_config/nvim/lua/remaps.lua
@@ -5,7 +5,7 @@ vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mZJ`z")
+vim.keymap.set("n", "J", "mZJ`Z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
@@ -13,9 +13,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", '"_dP')
 
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
-
-vim.keymap.set("n", "<leader>d", '"_d')
-vim.keymap.set("v", "<leader>d", '"_d')
+vim.keymap.set({"n", "v"}, "<leader>y", '"+y')
+vim.keymap.set({"n", "v"}, "<leader>d", '"_d')
