d1b9fd9b32b6bca9960c5af8ab6f639cbaa235a8
Gustavo Asai <gustavojun.asai@gmail.com>
2024-12-08 21:23:03 +0100

    add notification for macro recording in lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -5,13 +5,22 @@ return {
             options = {
                 theme = "nord",
                 disabled_filetypes = { 'neo-tree' },
-                component_separators = { left = '|', right = '|'},
-                section_separators = { left = '', right = ''},
+                component_separators = { left = '|', right = '|' },
+                section_separators = { left = '', right = '' },
             },
             sections = {
-                lualine_x = {'filetype'},
-                lualine_y = {'progress'},
-                lualine_z = {'location'},
+                lualine_x = {
+                    function()
+                        if vim.fn.reg_recording() ~= '' then
+                            return 'Recording @' .. vim.fn.reg_recording()
+                        else
+                            return ''
+                        end
+                    end,
+                    'filetype'
+                },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' },
             },
         })
     end,
