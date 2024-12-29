3ed6d106cac77345b186395b3bc48d104bd9fa43
Gustavo Asai <gustavojun.asai@gmail.com>
2024-12-29 17:26:56 +0100

    fix clashing keybinds

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -11,11 +11,11 @@ return {
         vim.keymap.set("n", "<C-p>", function() harpoon:list():select(1) end, { desc = "Harpoon select 1" })
         vim.keymap.set("n", "<C-m>", function() harpoon:list():select(2) end, { desc = "Harpoon select 2" })
         vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end, { desc = "Harpoon select 3" })
-        vim.keymap.set("n", "<C-i>", function() harpoon:list():select(4) end, { desc = "Harpoon select 4" })
+        vim.keymap.set("n", "<C-b>", function() harpoon:list():select(4) end, { desc = "Harpoon select 4" })
 
         vim.keymap.set("n", "<leader><C-p>", function() harpoon:list():replace_at(1) end, { desc = "Harpoon replace at 1" })
         vim.keymap.set("n", "<leader><C-m>", function() harpoon:list():replace_at(2) end, { desc = "Harpoon replace at 2" })
         vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end, { desc = "Harpoon replace at 3" })
-        vim.keymap.set("n", "<leader><C-i>", function() harpoon:list():replace_at(4) end, { desc = "Harpoon replace at 4" })
+        vim.keymap.set("n", "<leader><C-b>", function() harpoon:list():replace_at(4) end, { desc = "Harpoon replace at 4" })
     end,
 }
