13c962124e50485c8ee8965b43eaed89e62fca00
Gustavo Asai <gustavojun.asai@gmail.com>
2025-04-27 15:29:31 +0200

    move trouble to telescope

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -39,10 +39,16 @@ return {
 
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+            vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "List LSP symbols" })
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
             vim.keymap.set("n", "<leader>gp", builtin.git_files, { desc = "Git files" })
             vim.keymap.set("n", "<leader>gt", builtin.git_status, { desc = "Git status" })
-            vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = "Show references" })
+            vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Show references" })
+            vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Show definitions" })
+            vim.keymap.set("n", "<leader>xd", builtin.diagnostics, { desc = "Show diagnostics" })
+            vim.keymap.set("n", "<leader>xx", function()
+                builtin.diagnostics({ bufnr = 0 })
+            end, { desc = "Show diagnostics current buffer" })
         end,
     },
     {
